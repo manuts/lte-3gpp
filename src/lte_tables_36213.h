69c901b7b5334961ae463e5fe143af1ca40a91f4
Manu Thottathil Sreedharan <mthottathilsreed@apple.com>
2021-11-22 23:23:05 +0100

    add tables for UL_DAI computation

--- src/lte_tables_36213.h
+++ src/lte_tables_36213.h
@@ -23,12 +23,15 @@
 /*
  *  References
  *  [1] 3GPP TS 36.213 version 16.3.0 Release 16
+ *  [2] 3GPP TS 36.213 version 13.9.0 Release 13
+ *  [3] 3GPP TS 36.213 version 10.11.0 Release 10
  */
 
 #ifndef _LTE_TABLES_36213_H_
 #define _LTE_TABLES_36213_H_
 
 #include <vector>
+#include <array>
 
 #include "lte_consts.h"
 #include "lte_enums.h"
@@ -45,5 +48,32 @@ inline const std::vector<std::vector<std::vector<uint8_t>>> downlinkAssociationS
         {{},{},{7,},{7,},{5,},{},{},{7,},{7,},{},},
 }};
 
+// [2] Table 7.3-Y: Uplink association index k' for TDD
+inline const std::array<std::array<uint8_t, lte::consts::numSubframesPerRadioFrame>, lte::consts::numTddUlDlConfigurations> uplinkAssociationIndex_T7P3_Y =
+{{
+        // Note that table 7.3-Y does not have any entry for TDD UL/DL Configuration 0.
+        // For TDD UL/DL Configuration 1-6, PUSCH in subframe n is allocated by UL-Grant in subframe n-k'.
+        // For example, consider UL/DL Configuration 1:
+        // A PUSCH transmission in (frame, subframe) = (x, 2) has to be allocated by UL-Grant in (frame, subframe) = (x-1, 6).
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 6, 4, 0, 0, 0, 6, 4, 0},
+        {0, 0, 4, 0, 0, 0, 0, 4, 0, 0},
+        {0, 0, 4, 4, 4, 0, 0, 0, 0, 0},
+        {0, 0, 4, 4, 0, 0, 0, 0, 0, 0},
+        {0, 0, 4, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 7, 7, 5, 0, 0, 7, 7, 0},
+}};
+
+// [3] Table 8-2 k for TDD configurations 0-6
+inline const std::array<std::array<uint8_t, lte::consts::numSubframesPerRadioFrame>, lte::consts::numTddUlDlConfigurations> k_forTddConfigurations_T8_2 =
+{{
+        {4, 6, 0, 0, 0, 4, 6, 0, 0, 0},
+        {0, 6, 0, 0, 4, 0, 6, 0, 0, 4},
+        {0, 0, 0, 4, 0, 0, 0, 0, 4, 0},
+        {4, 0, 0, 0, 0, 0, 0, 0, 4, 4},
+        {0, 0, 0, 0, 0, 0, 0, 0, 4, 4},
+        {0, 0, 0, 0, 0, 0, 0, 0, 4, 0},
+        {7, 7, 0, 0, 0, 7, 7, 0, 0, 5},
+}};
 
 #endif /* _LTE_TABLES_36213_H_ */
